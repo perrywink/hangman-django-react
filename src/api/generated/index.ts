90ed8a30592bcba4345f54284bf2332329dd4864
perrywink <isaacyjc@gmail.com>
2024-10-26 16:56:29 +1100

    setup type generation

---
+++ src/api/generated/index.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { Game } from './models/Game';
+export type { GameGuess } from './models/GameGuess';
+export type { GameId } from './models/GameId';
+export type { PatchedGameGuess } from './models/PatchedGameGuess';
+export { StatusEnum } from './models/StatusEnum';
+
+export { GameService } from './services/GameService';
+export { GamesService } from './services/GamesService';
---
+++ src/api/generated/models/StatusEnum.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * * `InProgress` - In Progress
+ * * `Loss` - Loss
+ * * `Won` - Won
+ */
+export enum StatusEnum {
+    IN_PROGRESS = 'InProgress',
+    LOSS = 'Loss',
+    WON = 'Won',
+}
